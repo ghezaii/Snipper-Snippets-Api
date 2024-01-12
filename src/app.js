00aaa6df8cf76e5a1d952ab524e8a9d18be17eb2
ghezaii <ghezai@google.com>
2024-01-12 17:23:09 -0500

    add user route and require .env

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
+require('dotenv').config()
 const express = require("express")
 const app = express()
 const { router } = require("./routes/snippet")
+const { route } = require("./routes/user")
 
 const port = 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use("/snippet", router)
+app.use("/user", route)
 
 module.exports = app;
