58016cf242de5b4d0302c17e792a4151d0c033d7
ghezaii <ghezai@google.com>
2024-01-25 17:05:13 -0500

    alter routes to add requireAuth

--- src/routes/snippet.js
+++ src/routes/snippet.js
@@ -1,30 +1,31 @@
 const { Router } = require('express')
 const router = Router()
 const { encrypt, decrypt } = require('../utils/encrypt')
-const authorize = require('../middleware/authorize')
+const { requiresAuth } = require('express-openid-connect')
 
 const snippets = require('../seedData.json')
 
 let id = snippets.length
 
-router.get("/", authorize, (req, res) => {
+router.get("/", requiresAuth(), (req, res) => {
     const { language } = req.query
 
-    const decryptedSnippets = snippets.map(snippet => ({
-        ...snippet,
-        code: decrypt(snippet.code)
-    }))
+    // const decryptedSnippets = snippets.map(snippet => ({
+    //     ...snippet,
+    //     code: decrypt(snippet.code)
+    // }))
 
-    if (language) {
-        const filteredLanguages = snippets.filter(
-            snippet => snippet.language.toLowerCase() === language.toLowerCase()
-        )
-        return res.json(filteredLanguages)
-    }
+    // if (language) {
+    //     const filteredLanguages = decryptedSnippets.filter(
+    //         snippet => snippet.language.toLowerCase() === language.toLowerCase()
+    //     )
+    //     return res.json(filteredLanguages)
+    // }
+    // res.send(decryptedSnippets)
     res.send(snippets)
 })
 
-router.get("/:id", authorize, (req, res) => {
+router.get("/:id", requiresAuth(), (req, res) => {
     const snippetId = parseInt(req.params.id)
     const snippet = snippets.find(snippet => snippet.id === snippetId)
 
@@ -32,11 +33,11 @@ router.get("/:id", authorize, (req, res) => {
         return res.status(404).json({ error: 'Snippet not found' })
     }
 
-    snippet.code = decrypt(snippet.code)
+    //snippet.code = decrypt(snippet.code)
     res.json(snippet)
 })
 
-router.post("/", authorize, (req, res) => {
+router.post("/", requiresAuth(), (req, res) => {
     const { language, code } = req.body
 
     if (!language || !code) {
@@ -51,7 +52,8 @@ router.post("/", authorize, (req, res) => {
         code
     }
 
-    snippets.push({...snippet, code: encrypt(code)});
+    //snippets.push({...snippet, code: encrypt(code)});
+    snippets.push(snippet)
     res.status(201).json(snippet)
 })
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,50 +1,10 @@
 const route = require('express').Router()
-const basicAuth = require('../middleware/basicAuth')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const authorize = require('../middleware/authorize')
+const { requiresAuth } = require('express-openid-connect')
 
-const users = []
+//const users = []
 
-route.post('/', basicAuth, async (req, res) => {
-    const { email, password } = req.user
-    const id = users.length + 1
-
-    const salt = 10
-    const hash = await bcrypt.hash(password, salt)
-
-    const user = {
-        id,
-        email,
-        password: hash
-    }
-
-    users.push(user)
-    res.status(201).json({ id, email })
-})
-
-route.post('/login', basicAuth, async (req, res) => {
-    const user = users.find(user => user.email === req.user.email)
-
-    if (!user) {
-        return res.status(404).send({ error: 'User not found.'})
-    }
-
-    const result = await bcrypt.compare(req.user.password, user.password)
-
-    if (!result) {
-        return res.status(401).json({ error: 'Incorrect password' })
-    }
-
-    const payload = {id: user.id, email: user.email}
-
-    const accessToken = jwt.sign(payload, process.env['TOKEN_SECRET'])
-
-    res.json({ accessToken })
-})
-
-route.get('/', authorize, async (req, res) => {
-    res.json(req.user)
+route.get('/', requiresAuth(), async (req, res) => {
+    res.json(req.oidc.user)
 })
 
 module.exports = { route };
